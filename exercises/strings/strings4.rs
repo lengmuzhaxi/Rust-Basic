e93df622ba2c98a25fe8feaee4c62e76433836f7
lengmuzhaxi <2690497440@qq.com>
2025-01-21 13:13:37 +0800

    string exercises

--- exercises/strings/strings4.rs
+++ exercises/strings/strings4.rs
@@ -17,7 +17,7 @@ fn string(arg: String) {
 }
 
 fn main() {
-    ???("blue");
+   /*???("blue");
     ???("red".to_string());
     ???(String::from("hi"));
     ???("rust is fun!".to_owned());
@@ -26,5 +26,16 @@ fn main() {
     ???(&String::from("abc")[0..1]);
     ???("  hello there ".trim());
     ???("Happy Monday!".to_string().replace("Mon", "Tues"));
-    ???("mY sHiFt KeY iS sTiCkY".to_lowercase());
+    ???("mY sHiFt KeY iS sTiCkY".to_lowercase());*/
+        string_slice("blue");  // 字符串字面值是 &str 类型
+        string("red".to_string());  // to_string() 生成 String 类型
+        string(String::from("hi"));  // String::from 生成 String 类型
+        string("rust is fun!".to_owned());  // to_owned() 生成 String 类型
+        string("nice weather".into());  // into() 自动推断为 String 类型
+        string(format!("Interpolation {}", "Station"));  // format! 生成 String 类型
+        string_slice(&String::from("abc")[0..1]);  // 字符串切片，需要 &str
+        string_slice("  hello there ".trim());  // trim() 返回 &str
+        string("Happy Monday!".to_string().replace("Mon", "Tues"));  // replace() 生成 String 类型
+        string("mY sHiFt KeY iS sTiCkY".to_lowercase());  // to_lowercase() 生成 String 类型
+    
 }
